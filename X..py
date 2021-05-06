629e224fa4ee9677ca7573377e214d110a8b78fd
Clutchouz <83761878+Clutchouz@users.noreply.github.com>
2021-05-06 13:49:21 +0300

    Add files via upload

---
+++ X..py
@@ -0,0 +1,66 @@
+
+def pin_management(code, name):
+    print("This is your code " + code + " and your holder " + name)
+
+pin_management("1243", "xhack")
+pin_management("8124", "xxxx")
+
+
+def pin_management(code, name):
+    print("This is your code " + code + " and your holder " + name)
+
+pin_management("1243", "xhack")
+pin_management("8124", "xxxx")
+
+def pin_management(code, name):
+    print("This is your code " + code + " and your holder " + name)
+
+pin_management("1243", "xhack")
+pin_management("8124", "xxxx")
+
+def pin_management(code, name):
+    print("This is your code " + code + " and your holder " + name)
+
+pin_management("1243", "xhack")
+pin_management("8124", "xxxx")
+
+def pin_management(code, name):
+    print("This is your code " + code + " and your holder " + name)
+
+pin_management("1243", "xhack")
+pin_management("8124", "xxxx")
+
+
+phrase = "Hi world!"
+print(phrase.upper().isupper())
+
+phrase = "Hi world!"
+print(phrase[1])
+
+phrase = "Hi world!"
+print(phrase.index("H"))
+
+phrase = "Hi world!"
+print(phrase.replace("Hi", "_|_"))
+
+my_num = 696969
+print(str(my_num) + " shot nr.1" )
+
+from math import *
+
+my_num = -134
+print(floor(5.6))
+
+NR1 = input("Introdu bancnota: ")
+NR2 = input("Introdu moneda: ")
+result =(float(NR1) + float(NR2))
+
+print (result)
+
+id_pin = input("Enter your PIN: ")
+id_user = input("Enter your username: ")
+id_night = input("Enter your pulse: ")
+
+print ("Sold total: " + id_pin)
+print ("Username activ 20.01.2020.upper: " + id_user)
+print ("Amprenta miocard: 7" + id_night)
